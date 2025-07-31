d8ed42c2f0954fe2cccb47c3f8aa4b8e1f1de54f
Vincent <vlassiter@outlook.com>
2025-07-31 14:43:03 -0400

    remove basepath from src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-       <BrowserRouter basename="/thebigbeat-visual-journey">
+       <BrowserRouter >
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
